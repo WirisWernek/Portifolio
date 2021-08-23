ef4cae09e3d9d25151579cb5f1c6226707d155c7
Wiris Rafael Januario Wernek <rafaeljwerneck43@gmail.com>
2021-08-22 22:12:25 -0300

    Midificando email, adicionando arquivo de PHP e direcionando o formulário pra ele

---
+++ mail.php
@@ -0,0 +1,24 @@
+<?php
+if(isset($_POST['email']) && !empty($_POST['email'])){
+
+$nome = addslashes($_POST['name']);
+$email = addslashes($_POST['email']);
+$mensagem = addslashes($_POST['message']);
+
+$to = "rafaeljwerneck43@gmail.com";
+$subject = "Portifolio - Contato de $nome";
+$body = "Nome: ".$nome. "\r\n".
+        "Email: ".$email. "\r\n".
+        "Mensagem: ".$mensagem;
+
+$header = "From: wiris@000webhostapp.com"."\r\n"."Reply-To: ".$email."\e\n"."X=Mailer:PHP/".phpversion();
+
+if (mail($to,$subject,$body,$header)){
+    echo("Email enviado com sucesso!");
+}else{
+    echo("Ocorreu um erro ao enviar o email!");
+}
+
+}
+
+?>
